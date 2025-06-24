aadf20ddeed0fc43e553f48f6188b233b0b4eca1
VibhuDixit-2215001940 <vibhudixit.2215001940@gmail.com>
2025-06-24 10:02:16 +0530

    add Constructor/Default

--- Test.java
+++ Test.java
@@ -1,19 +1,69 @@
-// Class definition
-class Class_Name {
-    int a = 10;
-    String name = "Rahul Gandhi";
+// -------------------------------CLASS INTRO-----------------
+// // Class definition
+// class Class_Name {
+// int a = 10;
+// String name = "Rahul Gandhi";
 
-    // Method to show values
-    public void show() {
-        System.out.println(a + " " + name);
-    }
-}
+// // Method to show values
+// public void show() {
+// System.out.println(a + " " + name);
+// }
+// }
 
-// Main class
-public class Test {
-    public static void main(String[] args) {
-        // Object creation
-        Class_Name obj = new Class_Name();
-        obj.show(); // Output: 10 Rahul Gandhi
-    }
-}
+// // Main class
+// public class Test {
+// public static void main(String[] args) {
+// // Object creation
+// Class_Name obj = new Class_Name();
+// obj.show(); // Output: 10 Rahul Gandhi
+// }
+// }
+
+// ----------------------------CONSTRUCTOR-----------------
+// class Rampo {
+// int a;
+
+// Rampo() {
+// a = 10;
+// }
+
+// public void show() {
+// System.out.println(a);
+// }
+// }
+
+// class Test {
+// public static void main(String[] args) {
+// System.out.println("We are learning constructor:---");
+// Rampo a = new Rampo();
+// a.show();
+// }
+// }
+
+// ---------------------------DEFAULT CONSTRUCTOR------------
+// every class in java has a default constructor that is made by compiler if not
+// already present ....... and that constructor assigns default value too the
+// vars that might present in that class!!!!!!!
+// class Demo {
+// int a;
+// String str;
+
+// // Demo() { //Behind the scene compiler made it by default and initialise
+// // default values!!
+// // a = 0;
+// // str = null;
+// // }
+
+// public void show() {
+// System.out.println(a + " " + str);
+// }
+// }
+
+// class Test {
+// public static void main(String[] args) {
+// Demo a = new Demo();
+// a.show();
+// }
+// }
+
+// -----------------------------PARAMETERIZED CONSTRUCTOR--------------
